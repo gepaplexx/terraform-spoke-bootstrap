97c1278814222271c421387f26aad34b2a9e6d69
Clemens Kaserer <ckaserer@gmail.com>
2021-10-27 16:49:48 +0000

    quote your values...

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "vsphere_folder" "folder" {
 }
 
 resource vsphere_virtual_machine "vm" {
-  name             = test
+  name             = "test"
   resource_pool_id = "${data.vsphere_compute_cluster.cc.resource_pool_id}"
   datastore_id     = "${data.vsphere_datastore.ds.id}"
   folder           = "GP/${var.spoke_name}"
@@ -46,7 +46,7 @@ resource vsphere_virtual_machine "vm" {
 
   vapp {
     properties ={
-      hostname = test
+      hostname = "test"
       # user-data = base64encode(file("${path.module}/cloudinit/kickstart.yaml"))
     }
   }
