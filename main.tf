485778b10de3a4923a26f36b16907f93edf2fefe
Clemens Kaserer <ckaserer@gmail.com>
2021-10-27 16:47:58 +0000

    set hardcoded hostname

--- main.tf
+++ main.tf
@@ -13,11 +13,11 @@ resource "vsphere_folder" "folder" {
 }
 
 resource vsphere_virtual_machine "vm" {
-  name             = var.hostname
+  name             = test
   resource_pool_id = "${data.vsphere_compute_cluster.cc.resource_pool_id}"
   datastore_id     = "${data.vsphere_datastore.ds.id}"
   folder           = "GP/${var.spoke_name}"
-  
+
   num_cpus = 2
   memory   = 1024
   guest_id = "{data.vsphere_virtual_machine.template.guest_id}"
