32d2b9edf7a5d57f15e72a54de73b53558b75599
Clemens Kaserer <ckaserer@gmail.com>
2021-12-03 11:49:21 +0000

    updated data.tf

--- data.tf
+++ data.tf
@@ -1,8 +1,8 @@
-data "vsphere_datacenter" "dc" {
+data vsphere_datacenter "dc" {
   name = "${var.vmware_datacenter}"
 }
 
-data "vsphere_host" "host" {
+data vsphere_host "host" {
   count         = "${length(var.vmware_esxi_hosts)}"
   name          = "${var.vmware_esxi_hosts[count.index]}"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
